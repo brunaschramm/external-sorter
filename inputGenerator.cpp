fed34011e6bc41a438139041b926dc3630c0af27
Bruna Schramm <bruna.schramm@neemu.com>
2016-04-08 23:10:40 -0400

    Ajustando gerador de entrada - tava quebrando quando memoria disponivel era >= 16

--- inputGenerator.cpp
+++ inputGenerator.cpp
@@ -11,14 +11,8 @@ using namespace std;
 
 const static unsigned long int GB = 1024*1024*1024;
 
-void limparAmbiente()
-{
-	system("rm -rf input/");
-}
-
 void prepararAmbiente()
 {
-	limparAmbiente();
 	system("mkdir input/");
 }
 
@@ -27,37 +21,41 @@ int main(int argv, char *argc[])
 	int tam = 4;
 	if(argv != 2)
 	{
-		cout << "Gerando entrada de tamanho = [4 GB]" << endl;
+		cout << "Gerando entrada de tamanho = [4] GB" << endl;
 		cout << "Para gerar outro valor : " << argc[0] << " <qntd-em-GB>" << endl;
 	}
-	else tam = atoi(argc[1]);
-	
+	else
+            {
+                tam = atoi(argc[1]);
+                cout << "Gerando entrada de tamanho = [" << tam << "] GB" << endl;
+            }
+
 	if(tam < 0)
 	{
 		cout << "Tamanho do arquivo de ser pelo menos de [1 GB]" << endl;
 		exit(0);
 	}
-	
+
 	prepararAmbiente();
-	
+
 	int randValue = 0;
 
 	/* initialize random seed: */
 	srand (time(NULL));
-	
+
 	unsigned long int QTD_VALORES = (tam * GB) / 4;
-	
+
 	FILE *arqBin = fopen("input/input.bin", "w");
-	int i;
+	unsigned long int i;
 	for (i = 0; i < QTD_VALORES; i++)
 	{
 		randValue = rand();
 		fwrite (&randValue, sizeof(unsigned int), 1, arqBin);
 	}
-	
+
 	fclose(arqBin);
-	
+
 	cout << "Arquivo gerado com sucesso! Total de valores = " << i << endl;
-	
+
 	return 0;
 }
